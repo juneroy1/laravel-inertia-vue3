a21b15e5ffc55d452ecf37d9b20f2308fb1cb0f1
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2025-01-15 10:12:41 +0800

    update routes and app file

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,1 +1,11 @@
-<template><h1>Hello from Vue 3 with Inertia.js!</h1></template>
+<template>
+    <div>
+        <h1>Hello from Vue 3 with Inertia.js!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Welcome",
+};
+</script>
