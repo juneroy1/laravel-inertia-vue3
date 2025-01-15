7643efe02c477eb3cdf1a79b2032db7a0a2be310
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2025-01-15 15:52:12 +0800

    added new route for payment success and cancel

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
+use App\Http\Controllers\PaymentController;
 Route::get('/', function () {
     sleep(2);
-    return Inertia::render('Home');
+    return Inertia::render('Home', [
+        'stripeKey' => config('services.stripe.key'),
+    ]);
 });
 
 Route::get('/contact', function () {
@@ -17,3 +19,19 @@ Route::get('/settings', function () {
     sleep(2);
     return Inertia::render('Settings');
 });
+
+
+Route::get('/payment/success', function () {
+    return Inertia::render('Payment/Success');
+});
+
+Route::get('/payment/cancel', function () {
+    return Inertia::render('Payment/Cancel');
+});
+
+
+
+Route::get('/payment/success', [PaymentController::class, 'success'])->name('payment.success');
+Route::get('/payment/cancel', [PaymentController::class, 'cancel'])->name('payment.cancel');
+Route::post('/create-checkout-session', [PaymentController::class, 'createCheckoutSession'])->name('checkout.session');
+
