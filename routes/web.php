4cc9b9226ffb1c096459abdf48bed33d42b65cb5
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2025-01-15 13:53:48 +0800

    install tailwind css

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,16 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
+    sleep(2);
     return Inertia::render('Home');
 });
 
 Route::get('/contact', function () {
+    sleep(2);
     return Inertia::render('Contact');
 });
 
 Route::get('/settings', function () {
+    sleep(2);
     return Inertia::render('Settings');
 });
