23c0cf7d414cc37d9d53255e5bbcc7a69fc4df5f
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2025-01-15 15:52:00 +0800

    integrate stripe cdn

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        <script src="https://js.stripe.com/v3/"></script>
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
