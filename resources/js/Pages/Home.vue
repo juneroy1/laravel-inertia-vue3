762e1a8055e0eba65b49f3f4e27437c88437ee16
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2025-01-15 13:30:52 +0800

    added nav component

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,22 +1,17 @@
 <template>
     <div>
         <h1>Home</h1>
-        <nav>
-            <ul>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/contact">Contact</Link></li>
-                <li><Link href="/settings">Settings</Link></li>
-            </ul>
-        </nav>
+        <Nav/>
     </div>
 </template>
 
 <script>
 import { Link } from '@inertiajs/inertia-vue3';
+import Nav from '../shared/Nav.vue';
 
 
 export default {
     name: "Home",
-    components: {Link}
+    components: {Link, Nav}
 };
 </script>
